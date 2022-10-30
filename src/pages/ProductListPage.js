07ef84a962e2208f76a774ad2da346dec1b0315d
Malgorzata Nowak <nowak.em@interia.eu>
2022-10-30 20:40:09 +0100

    initial commit

---
+++ src/pages/ProductListPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/ProductListPage.css';
+
+const products = ["car", "bike", "motorcycle"];
+
+const ProductListPage = () => {
+
+  const list = products.map(product => (
+    <li key={product}>
+      <Link to={`/product/${product}`}>{product}</Link>
+    </li>
+  ))
+
+  return (
+    <div className="products">
+
+      <h3>Lista produktów</h3>
+      <hr />
+      <div>
+        <ul>
+          {list}
+        </ul>
+      </div>
+    </div>
+
+  );
+}
+
+export default ProductListPage;
